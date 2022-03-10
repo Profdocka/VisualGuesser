f9c289bcff64efa013076c1a6554986dd493b84c
Profdocka <tobiasluca@ewe.net>
2022-03-10 16:02:59 +0100

    3 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { QuizComponent } from './components/quiz/quiz.component';
-
+import { GameDataService } from './data/game-data.service';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, QuizComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HelloComponent, QuizComponent],
+  bootstrap: [AppComponent],
+  providers: [GameDataService],
 })
-export class AppModule { }
+export class AppModule {}
