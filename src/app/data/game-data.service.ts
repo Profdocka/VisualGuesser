ee5680d2ab841954d0162b04860a691bdf7d72c1
Profdocka <tobiasluca@ewe.net>
2022-03-10 15:55:29 +0100

    1 file modified

--- src/app/data/game-data.service.ts
+++ src/app/data/game-data.service.ts
@@ -9,17 +9,19 @@ export class GameDataService {
   CorrectGame: Game = new Game(null, null, null);
   WrongGames: Game[] = [];
 
-  constructor(private http: HttpClient) {
-    const url = "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/"; 
-    var header = new HttpHeaders()
-    .set('Client-ID', '56pmsmf23lb6a8rn0z6t8vvg47r0a2')
-    .set('Authorization', 'Bearer ud22j0xr53lb06mep6au7m8mxcogx3');
-    var params = new HttpParams().set('fields', 'name').set('search', 'SIX');
+  constructor(private http: HttpClient) { 
   }
 
+  url = "https://cors-anywhere.herokuapp.com/https://api.igdb.com/v4/games/"; 
+
+  header = new HttpHeaders()
+    .set('Client-ID', '56pmsmf23lb6a8rn0z6t8vvg47r0a2')
+    .set('Authorization', 'Bearer ud22j0xr53lb06mep6au7m8mxcogx3');
+  params = new HttpParams().set('fields', 'name').set('search', 'SIX');
+
   //Sendet eine Request an die Videogame-API und liefert ein Observable zurück
-  getData(): Observable {
-    this.http.get()
+  getData(): Observable<any> {
+    return this.http.get(this.url, {headers: this.header, params: this.params})
   }
 
 }
