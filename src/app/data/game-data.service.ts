6a9af952352c1b8b5a31e9ab9d357e502cbc1a55
Profdocka <tobiasluca@ewe.net>
2022-03-10 15:40:19 +0100

    1 file modified

--- src/app/data/game-data.service.ts
+++ src/app/data/game-data.service.ts
@@ -1,8 +1,12 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class GameDataService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    CorrectGame: 
+
+   }
 
 }
